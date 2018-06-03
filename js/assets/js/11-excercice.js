a582f8a7e6c63dee2e911b446d371c308ad75612
iki85 <riki.tinoza@hotmail.com>
2018-06-03 21:53:09 +0200

    03/06

--- js/assets/js/11-excercice.js
+++ js/assets/js/11-excercice.js
@@ -32,10 +32,10 @@ var PremierTrismestre = [
     },
     {
         prenom: "Riki",
-        nom: "Ndinga",
+        nom: "NDINGA",
         moyenneMatiere: {
             francais: 11,
-            anglais: 16,
+            espagnol: 16,
             physique: 9,
         }
     },
@@ -79,7 +79,7 @@ for( i = 0; i < PremierTrismestre.length ; i++ ){
     var moyenneGeneral= 0;
     var moyenneDec = 0
 
-    w( 'Moyenne de ' + PremierTrismestre[i].prenom + ' ' + PremierTrismestre[i].nom )
+    w( PremierTrismestre[i].prenom + ' ' + '<span style="text-transform: Uppercase;">' +PremierTrismestre[i].nom+ '</span>' )
     w( '<ul style="list-style: none;">');
     while ( j < Object.keys(PremierTrismestre[i].moyenneMatiere).length){
         w( '<li style="text-transform: capitalize;">' + Object.keys(PremierTrismestre[i].moyenneMatiere)[j] + ' : '+ Object.values(PremierTrismestre[i].moyenneMatiere)[j]+ '</li>');
